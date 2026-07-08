33c0de805e3e0769e0333d665e9c4be64227c4c9
Alisha <alisha230207@gmail.com>
2026-07-08 16:22:34 +0530

    updated lc_1456

--- LC_1456.java
+++ LC_1456.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 //sliding window ------- substrings
 
-class Solution {
+class LC_1456 {
     public static boolean isVowel(char ch){
         return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
     }
 
-    public int maxVowels(String s, int k) {
+    public static int maxVowels(String s, int k) {
         int count=0;
-        s.toLowerCase();
+        // s = s.toLowerCase(); 
 
         for(int i=0;i<k;i++){
             if(isVowel(s.charAt(i))) count++;
@@ -23,4 +23,8 @@ class Solution {
 
         return maxVowel;
     }
+    public static void main(String[] args) {
+        System.out.println(maxVowels("Aisha",3));
+    }
 }
+
