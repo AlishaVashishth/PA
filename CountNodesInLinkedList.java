94cf95efaa10ba41808e7f83aac839f1a2a2bf6f
Alisha <alisha230207@gmail.com>
2026-08-17 17:14:13 +0530

    count nodes

--- CountNodesInLinkedList.java
+++ CountNodesInLinkedList.java
@@ -8,20 +8,20 @@ public class CountNodesInLinkedList {
             this.next=null;
         }
     }
-    public static void countNodes(Node head){
+    public static int countNodes(Node head){
         Node temp=head;
         int count=0;
         while(temp!=null){
             count++;
             temp=temp.next;
         }
-        System.out.println(count);
+        return count;
     }
     public static void main(String[] args){
         Node head = new Node(10);
         head.next = new Node(20);
         head.next.next = new Node(30);
         head.next.next.next = new Node(40);
-        countNodes(head);
+        System.out.println(countNodes(head));
     }
 }
