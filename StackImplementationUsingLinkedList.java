5498ce667e50012871037365da89bf5331c003e4
Alisha <alisha230207@gmail.com>
2026-09-14 16:55:44 +0530

    stack implementation using Linked List

---
+++ StackImplementationUsingLinkedList.java
@@ -0,0 +1,54 @@
+public class StackImplementationUsingLinkedList {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    static class Stack{
+        Node top;
+        void push(int val){
+            Node new_node = new Node(val);
+            new_node.next=top;
+            top=new_node;
+        }
+        int pop(){
+            if(top==null){
+                System.out.println("Stack Underflow");
+                return -1;
+            }
+            int val=top.data;
+            top=top.next;
+            return val;
+        }
+        int peek(){
+            if(top==null){
+                return -1;
+            }
+            return top.data;
+        }
+        void display(){
+            Node temp=top;
+            while(temp!=null){
+                System.out.print(temp.data+" ");
+                temp=temp.next;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        st.push(50);
+        st.push(60);
+        st.display();
+
+        System.out.println("Popped: "+st.pop());
+        System.out.println("Top element is: "+st.peek());
+        st.display();
+    }
+}
