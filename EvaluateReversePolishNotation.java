b77172842265373f5e2cf5d61dcb2c6338c46e7e
AlishaVashishth <alisha230207@gmail.com>
2026-09-21 16:35:11 +0530

    Leetcode 150

---
+++ EvaluateReversePolishNotation.java
@@ -0,0 +1,22 @@
+//Leetcode 150
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        int res=0;
+        for(String token:tokens){
+            if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")){
+                int b = stack.pop();
+                int a = stack.pop();
+                if(token.equals("+")) stack.push(a+b);
+                else if(token.equals("-")) stack.push(a-b);
+                else if(token.equals("*")) stack.push(a*b);
+                else stack.push(a/b);
+            }
+            else{
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
